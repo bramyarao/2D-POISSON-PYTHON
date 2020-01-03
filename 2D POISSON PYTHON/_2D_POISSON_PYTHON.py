880cddd41a3472e20e4c76ea377c6c331f45f967
Ramya Basava <bramyarao@gmail.com>
2020-01-02 20:52:16 -0800

    Initial commit

--- 2D POISSON PYTHON/_2D_POISSON_PYTHON.py
+++ 2D POISSON PYTHON/_2D_POISSON_PYTHON.py
@@ -1,1 +1,8 @@
+#=========================================================
+# NUMERICAL ANALYSIS: USING MESHFREE METHOD
+
+# USING THE REPRODUCING KERNEL COLLOCATION METHOD TO SOLVE 
+# THE 2D POISSONS PROBLEM
+#=========================================================
+
 
