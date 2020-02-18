4d4277d78ae59d4203c8aa25fdb296626ee917a5
Ramya Basava <bramyarao@gmail.com>
2020-02-18 00:13:40 -0800

    Complete

--- 2D POISSON PYTHON/_2D_POISSON_PYTHON.py
+++ 2D POISSON PYTHON/_2D_POISSON_PYTHON.py
@@ -13,11 +13,12 @@ import math
 # Files
 import forming_NS_NC
 import forming_A
+import ShapeFunction
 
 #-------------------------
 #INPUT PARAMETERS
 #-------------------------
-showPlot = False #Plotting is done if true
+showPlot = True #Plotting is done if true
 printStatements = True #Printing is done if true
 
 #Domain
@@ -26,7 +27,8 @@ xdim2 = 1.0
 ydim1 = 0.0
 ydim2 = 1.0
 
-num_pts = np.array([5,5,5,5], float)
+num_pts = np.array([10,10,40,40], float)
+#num_pts = np.array([5,5,5,5], float)
 #print(num_pts.dtype)
 
 # No. of Source points in the each direction
@@ -71,11 +73,98 @@ sq_alphah = 1.0     # Weight for the natural boundary
 # Forming A matrix
 #-------------------------
 A1 = forming_A.part_of_NI(NC, NS, ss)
+A2 = forming_A.part_of_NEB(NEB,NS,ss,sq_alphag)
+
+A = np.concatenate((A1,A2))
 
 
+#-------------------------
+# Forming b vector 
+#-------------------------
 
+b = np.zeros((no_NC+no_NEB,1))
+no_int = NC.shape[0]
+int_1 = 0
 
+# INTERIOR force term
+for int_2 in range(no_int):
+    xtemp = NC[int_2,0]
+    ytemp = NC[int_2,1]        
+    b[int_1] = b[int_1] + (xtemp**2 + ytemp**2)*math.exp(xtemp*ytemp)       
+    int_1 += 1
 
+# int_1 is getting incremented
+# EB force terms
+
+for int_2 in range(NEB.shape[0]):
+    xtemp = NEB[int_2,0]
+    ytemp = NEB[int_2,1]        
+    b[int_1] = b[int_1] + sq_alphag*math.exp(xtemp*ytemp)         
+    int_1 += 1
+
+#-------------------------
+# Solving the system
+#------------------------
+
+AT = np.transpose(A)
+Afinal = np.matmul(AT,A)
+bfinal = np.matmul(AT,b)
+
+a = np.linalg.solve(Afinal,bfinal) # Least squares solution ,rcond=None
+
+#-----------------------------------------------------------------------
+# Comparing Solutions Along the Diagonal through (0,0) & (1,1)
+#-----------------------------------------------------------------------
+
+x_con = np.linspace(xdim1,xdim2,20)
+y_con = np.linspace(ydim1,ydim2,20)
+
+u_con = np.zeros((len(x_con),1))
+u_exact_con = np.zeros((len(x_con),1))
+
+for int1 in range(len(x_con)):
+    x = x_con[int1]
+    y = y_con[int1]
+    P = ShapeFunction.required_nodes(x,y,NS,ss)
+    SI = ShapeFunction.SF_2D(x,y,NS,P,ss)
+
+    # for getting uh value of u_approx at each of the points (x,y)
+    u_con[int1,0] = np.matmul(SI,a)
+
+    # Finding u_exact at the point (x,y)
+    u_exact_con[int1,0] = math.exp(x*y)
+
+# Diagonal length
+DL = np.zeros((len(x_con),1))
+for int6 in range(len(x_con)):
+    DL[int6,0]  = math.sqrt((x_con[int6])**2 + (y_con[int6])**2)
+
+#-----------------------------------------------------------------------
+# Plotting the numerical solution
+#-----------------------------------------------------------------------
+uScatter = np.zeros((NC.shape[0],1))
+uScatter_exact = np.zeros((NC.shape[0],1))
+
+for int1 in range(NC.shape[0]):
+    x = NC[int1,0]
+    y = NC[int1,1]
+    P = ShapeFunction.required_nodes(x,y,NS,ss)
+    SI = ShapeFunction.SF_2D(x,y,NS,P,ss)
+    
+    # for getting uh value of u_approx at each of the points (x,y)
+    uScatter[int1,0] = np.matmul(SI,a)
+        
+    # Finding u_exact at the point (x,y)
+    uScatter_exact[int1,0] = math.exp(x*y)
+
+#-------------------------
+# PRINTING
+#-------------------------
+if (printStatements == True):
+    print('Source points %d' %NS.shape[0])
+    print('Collocation points %d' %NC.shape[0])
+    print('Interior collocation points %d' %NI_c.shape[0])
+    print('EB collocation points %d' %NEB.shape[0])
 
 
 #-------------------------
@@ -100,8 +189,35 @@ if showPlot == True:
     plt.axis('equal')
     plt.title("Collocation Points")
     plt.legend()
+    plt.show()
+
+    plt.figure(3)
+    plt.plot(DL, u_con, 'ko', label='RKCM')
+    plt.plot(DL, u_exact_con, 'k--', label='Analytical')
+    plt.xlabel("Diagonal length")
+    plt.ylabel("u(x,y)")
+    plt.xlim(0.0,1.5)
+    plt.ylim(0.9,3.0)
+    plt.legend()
+    plt.show()
+
+    plt.figure(4)
+    fig4=plt.scatter(NC[:,0], NC[:,1], c=uScatter[:,0],cmap='hsv')
+    plt.colorbar(fig4)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("u(x,y) Numerical (RKCM)")
+    plt.show()
+
+    plt.figure(5)
+    fig5=plt.scatter(NC[:,0], NC[:,1], c=uScatter_exact[:,0],cmap='hsv')
+    plt.colorbar(fig5)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("u(x,y) Analytical (Exact)")
     interactive(False)
     plt.show()
+
 
     # For Saving: plt.savefig('ScatterPlot.png')
     # To show all figures at a time:
--- 2D POISSON PYTHON/forming_A.py
+++ 2D POISSON PYTHON/forming_A.py
@@ -6,8 +6,6 @@ import ShapeFunction
 
 # For interior points
 
-import ShapeFunction
-
 def part_of_NI(NC, NS, ss):
 
     A = np.zeros((NC.shape[0], NS.shape[0]))
@@ -24,9 +22,39 @@ def part_of_NI(NC, NS, ss):
         SIxx = ShapeFunction.DSF_xx(x,y,NS,P,ss)
         SIyy = ShapeFunction.DSF_yy(x,y,NS,P,ss)
 
+        #Looping over the no. of source pts.
+        for int_2 in range(NS.shape[0]): 
+            a_inter = SIxx[0,int_2] + SIyy[0,int_2]
 
+            # Arranging in in the matrix
+            A[int_row,int_col] = A[int_row,int_col] + a_inter
+            int_col += 1
 
-
+        int_col = 0
+        int_row += 1
 
     return A
 
+def part_of_NEB(NEB,NS,ss,sq_alphag):
+    A = np.zeros((NEB.shape[0], NS.shape[0]))
+    int_row = 0  # Row counter for A
+    int_col = 0  # Column counter for A
+
+    #Looping over the no. of boundary collocation pts. in the domain
+    for int_1 in range(NEB.shape[0]):
+        x = NEB[int_1,0]
+        y = NEB[int_1,1]
+        P = ShapeFunction.required_nodes(x,y,NS,ss)
+        SI = ShapeFunction.SF_2D(x,y,NS,P,ss)
+
+        for int_2 in range(NS.shape[0]):
+            a_inter = sq_alphag * SI[0,int_2]   # weight on the EB
+
+            # Arranging in in the matrix
+            A[int_row,int_col] = A[int_row,int_col] + a_inter
+            int_col += 1
+
+        int_col = 0
+        int_row += 1
+
+    return A
